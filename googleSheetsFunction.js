6820dee28c27dea80412382be0e36730c2a48895
Rachel <rachel.chloe.gibbs@gmail.com>
2018-08-13 12:33:02 -0500

    Create googleSheetsFunction.js

---
+++ googleSheetsFunction.js
@@ -0,0 +1,11 @@
+function onEdit(e) {
+
+{
+
+var url = 'https://groker.initialstate.com/api/events?accessKey=INSERT_ACCESS_KEY_HERE&bucketKey=INSERT_BUCKET_KEY_HERE&TimeSpent=' + e;
+
+var response = UrlFetchApp.fetch(url);
+
+}
+
+}
